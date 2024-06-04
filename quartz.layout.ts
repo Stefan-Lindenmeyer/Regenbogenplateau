81d6959fce1c577d33eec736889d6229e5985234
H-GUI <54667521+H-GUI@users.noreply.github.com>
2024-06-05 01:19:58 +0200

    Quartz sync: Jun 5, 2024, 1:19 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -11,16 +11,18 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.Search(),
+    Component.Darkmode(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
+    //Component.PageTitle(),
+    //Component.MobileOnly(Component.Spacer()),
+    
+    
     Component.Explorer(),
     //Component.DesktopOnly(Component.Explorer()),
   ],
