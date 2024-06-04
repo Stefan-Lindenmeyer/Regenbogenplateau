32fcdf7cfc9d7860f6d26121d5ba44cfa5a21f46
H-GUI <54667521+H-GUI@users.noreply.github.com>
2024-06-05 01:11:18 +0200

    Quartz sync: Jun 5, 2024, 1:11 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,15 +18,16 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
+    //Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
+    //omponent.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    //Component.Backlinks(),
   ],
 }
 
